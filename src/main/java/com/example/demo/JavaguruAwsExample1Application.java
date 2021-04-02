798ba478098529ff576f5d22472240ba1010a93b
ankit <ashumca108@gmail.com>
2021-04-02 23:59:32 +0530

    first commit

---
+++ src/main/java/com/example/demo/JavaguruAwsExample1Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaguruAwsExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaguruAwsExample1Application.class, args);
+		System.out.println("hello how are you");
+	}
+
+}
