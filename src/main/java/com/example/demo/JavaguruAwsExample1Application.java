97f7dab7b83e2e63260616748500cdb5e6b8aa03
dev-ankit-dev <62471092+dev-ankit-dev@users.noreply.github.com>
2021-08-15 20:36:02 +0530

    Update JavaguruAwsExample1Application.java

--- src/main/java/com/example/demo/JavaguruAwsExample1Application.java
+++ src/main/java/com/example/demo/JavaguruAwsExample1Application.java
@@ -9,13 +9,7 @@ public class JavaguruAwsExample1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(JavaguruAwsExample1Application.class, args);
 		System.out.println("hello how are you");
-		System.out.println("hello how are you");
-		System.out.println("hello how are you");
-		System.out.println("hello how are you");
 		
-				System.out.println("hello how are you");
-		System.out.println("hello how are you");
-		System.out.println("hello how are you");csacsadca
 		
 		
 		
