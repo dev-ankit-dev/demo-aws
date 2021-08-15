709b56dc25871bd044b841feb5313d130218576e
dev-ankit-dev <62471092+dev-ankit-dev@users.noreply.github.com>
2021-08-15 20:46:24 +0530

    Update JavaguruAwsExample1Application.java

--- src/main/java/com/example/demo/JavaguruAwsExample1Application.java
+++ src/main/java/com/example/demo/JavaguruAwsExample1Application.java
@@ -8,12 +8,9 @@ public class JavaguruAwsExample1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaguruAwsExample1Application.class, args);
-		System.out.println("hello how are you");
-		System.out.println("i am in  test branch");
+
 		System.out.println("hello how are you ankit");
-		System.out.println("i am in main brANCH");
-		System.out.println("i am in main brANCH");
-		System.out.println("i am in main brANCH");
+	
 		
 
 		
